5aa4f5fb4ff5d4f2d6fe023324b83045f3660057
Deepak <deepaksurya642@gmail.com>
2024-01-28 17:17:31 +0530

    developed the sidebar UI

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-3xl text-green-500">Hello (site)</div>;
+  return <div className="text-3xl text-green-500">Main Content</div>;
 }
