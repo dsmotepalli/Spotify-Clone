62c726a1268f6facfbb59b48945352308745e8e8
Deepak <deepaksurya642@gmail.com>
2024-01-29 13:28:09 +0530

    UI setup done for the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Sidebar>
-        <body className={font.className}>{children}</body>
-      </Sidebar>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
   );
 }
---
+++ components/Library.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
+const Library = () => {
+  const onCLick = () => {
+    //Hnadle upload
+  };
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 font-medium text-lg">Your Library</p>
+        </div>
+        <AiOutlinePlus onClick={onCLick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs</div>
+    </div>
+  );
+};
+
+export default Library;
